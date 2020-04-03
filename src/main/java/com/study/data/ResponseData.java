046088e15fd0dfb7656717a1b3b6927b817f5da8
mthomaz <mthomaz@uolinc.com>
2020-04-02 21:54:38 -0300

    Initial Commit

---
+++ src/main/java/com/study/data/ResponseData.java
@@ -0,0 +1,14 @@
+package com.study.data;
+
+public class ResponseData {
+
+    private int intValue;
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
+}
---
+++ src/main/java/com/study/nettyimplementation/NettyImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.study.nettyimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyImplementationApplication.class, args);
+	}
+
+}
